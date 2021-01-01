88af37297ea5e77b9232958bd7989e8da7598668
Li Jie <lijie.leow@sjsu.edu>
2021-01-01 13:12:47 +0800

    add npm scripts, eslint, Prettier, gitignore

--- src/app.js
+++ src/app.js
@@ -1,58 +1,60 @@
 // -------------- FIRST OF ALL -------------------------//
 const Pet = () => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, "Luna"),
-        React.createElement("h2", {}, "Dog"),
-        React.createElement("h2", {}, "Havanese")
-    ]);
-
-}
-
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Luna"),
+    React.createElement("h2", {}, "Dog"),
+    React.createElement("h2", {}, "Havanese"),
+  ]);
+};
 
 //it is like a rubber stamp
-const App = () => React.createElement("div", {}, [React.createElement("h1", {}, "Adopt Me!" ),
-React.createElement(Pet),
-React.createElement(Pet),
-React.createElement(Pet)
+const App = () =>
+  React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet),
+    React.createElement(Pet),
+    React.createElement(Pet),
+  ]);
 
-
-]);  
-
-//this is now u stamp the stamp 
+//this is now u stamp the stamp
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
 
 //-----------------------------PASSING IN COMPONENT PROPS ----------------------------//
 
-const Pet2 = props =>{
-    return React.createElement("div", {},[
-        React.createElement("h1", {}, props.name),
-        React.createElement("h2", {}, props.animal),
-        React.createElement("h2", {}, props.breed)
+const Pet2 = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+};
 
-
-    ]);
-}
-
-const App2 = () => React.createElement("div", {}, [React.createElement("h1", {}, "Adopt Me!" ),
-React.createElement(Pet, {name:"Luna", animal:"Dog", breed:"Havanese"}),
-React.createElement(Pet, {name:"Pepper", animal:"Bird", breed:"Cockatiel"}),
-React.createElement(Pet, {name:"Doink", animal:"Cat", breed:"Mixed"})
-
-]);  
+const App2 = () =>
+  React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel",
+    }),
+    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mixed" }),
+  ]);
 
 ReactDOM.render(React.createElement(App2), document.getElementById("root"));
 
 //--------------------------- DESTRUCTURING PROPS ------------------------------//
-const Pet3 = ({name,animal,breed}) =>{
-    return React.createElement("div", {},[
-        React.createElement("h1", {}, name),
-        React.createElement("h2", {}, animal),
-        React.createElement("h2", {}, breed)
-
-
-    ]);
-}
-
+const Pet3 = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed),
+  ]);
+};
 
 /** --------------------  NOTES -------------------------
  * React.createElement returns an react element (in lesson 1&2, a tag name string)
