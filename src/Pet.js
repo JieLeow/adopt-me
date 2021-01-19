d6d459a64fec8fe2df5bbc51bdb447b5701e9354
Li Jie <lijie.leow@sjsu.edu>
2021-01-19 18:25:09 +0800

    make async calls to PetfinderAPI & update Pet component with props

--- src/Pet.js
+++ src/Pet.js
@@ -1,21 +1,45 @@
 import React from "react";
-
-export default function Pet({ name, animal, breed }) {
-  //why export default? because can be imported with any name
-  //   return React.createElement("div", {}, [
-  //     React.createElement("h1", {}, name),
-  //     React.createElement("h2", {}, animal),
-  //     React.createElement("h2", {}, breed),
-  //   ]);
+export default function Pet({ name, animal, breed, media, location, id }) {
+  let hero = "http://placecorgi.com/300/300";
+  if (media.length) {
+    hero = media[0].small;
+  }
 
   return (
-    //this div gets translated to the code above, only 1 div because only can return 1 thing.
-
-    <div>
-      <h1>Name: {name.toUpperCase()}</h1>
-      <h2>{animal}</h2>
-      <h2>{breed}</h2>
-    </div>
-    //note: curly braces are for js expressions (RHS of js)
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>
+          {animal} - {breed} - {location}
+        </h2>
+      </div>
+    </a>
   );
 }
+
+//========================= OLD React Code====================================
+// import React from "react";
+
+// export default function Pet({ name, animal, breed }) {
+//   //props destructuring
+//   //why export default? because can be imported with any name
+//   //   return React.createElement("div", {}, [
+//   //     React.createElement("h1", {}, name),
+//   //     React.createElement("h2", {}, animal),
+//   //     React.createElement("h2", {}, breed),
+//   //   ]);
+
+//   return (
+//     //this div gets translated to the code above, only 1 div because only can return 1 thing.
+
+//     <div>
+//       <h1>Name: {name.toUpperCase()}</h1>
+//       <h2>{animal}</h2>
+//       <h2>{breed}</h2>
+//     </div>
+//     //note: curly braces are for js expressions (RHS of js)
+//   );
+// }
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,25 +1,46 @@
 import React, { useState, useEffect } from "react";
 import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
+import Results from "./Results";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
-
-  // const [animal, setAnimal] = useState("dog");
-  // const [breed, setBreed] = useState("");
   const [breeds, setBreeds] = useState([]); //requests from the api, but for now its empty
-
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [pets, setPets] = useState([]); //first request things to api will have no pets there;
+
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal,
+    });
+
+    setPets(animals || []);
+  }
 
   useEffect(() => {
     // pet.breeds("dog").then(console.log, console.error); //returns a promise,
-  });
+    setBreeds([]); //clears the breeds up
+    setBreed(""); //
+
+    pet.breeds(animal).then(({ breeds }) => {
+      //{breeds} is destructuring
+      const breedStrings = breeds.map(({ name }) => name);
+      setBreeds(breedStrings); //setBreeds state to an array of breed names
+    }, console.error);
+  }, [animal, setBreed, setBreeds]); //react dependency list, useEffect only runs when these changes
 
   return (
     <div className="search-params">
       <h1>{location}</h1>
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location
           <input
@@ -33,6 +54,7 @@ const SearchParams = () => {
         <BreedDropdown />
         <button>Submit</button>
       </form>
+      <Results pets={pets} />
     </div>
   );
 };
