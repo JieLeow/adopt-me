05efda9e048eb2df961bfbc5361c841e23713bf1
Li Jie <lijie.leow@sjsu.edu>
2021-01-19 19:03:40 +0800

    add Reach Router library

--- src/app.js
+++ src/app.js
@@ -1,37 +1,24 @@
 import React from "react";
 // import { render } from "react-dom";
+import { Router, Link } from "@reach/router";
 import ReactDOM from "react-dom";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
-  // return React.createElement("div", {}, [
-  //   React.createElement("h1", {}, "Adopt Me!"),
-  //   React.createElement(Pet, {
-  //     name: "Luna",
-  //     animal: "Dog",
-  //     breed: "Havanese",
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Pepper",
-  //     animal: "Bird",
-  //     breed: "Cockatiel",
-  //   }),
-  //   React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" }),
-  // ]);
-  // return (
-  //   //parentheses cuz if js sees a new line it just ends there
-  //   <div>
-  //     <h1>Adopt Me!</h1>
-  //     <Pet name="Luna" animal="Dog" breed="Havanese" />
-  //     <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-  //     <Pet name="Doink" animal="Cat" breed="Mix" />
-  //   </div>
-  // );
   return (
-    <div>
-      <h1 id="something-important">Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <React.StrictMode>
+      <div>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
+      </div>
+    </React.StrictMode>
   );
 };
 
