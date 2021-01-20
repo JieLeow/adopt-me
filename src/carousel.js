24fb473a455c80f9af92dc446c20c40579f8d3c4
Li Jie <lijie.leow@sjsu.edu>
2021-01-20 16:18:57 +0800

    add route to Details and create image Carousel

---
+++ src/carousel.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      photos: [],
+      active: 0,
+    };
+
+    this.handleIndexClick = this.handleIndexClick.bind(this);
+  }
+
+  static getDerivedStateFromProps({ media }) {
+    let photos = ["http://placecorgi.com/600/600"];
+
+    if (media.length) {
+      photos = media.map(({ large }) => large);
+    }
+    return { photos };
+  }
+
+  handleIndexClick(event) {
+    this.setState({
+      active: +event.target.dataset.index, //unary plus: turns string into number
+    });
+  }
+
+  render() {
+    const { photos, active } = this.state;
+    return (
+      <div className="carousel">
+        <img src={photos[active]} alt="animal" />
+        <div className="carousel-smaller">
+          {photos.map((photo, index) => (
+            <img //bad practice actually
+              key={photo}
+              onClick={this.handleIndexClick}
+              data-index={index}
+              src={photo}
+              className={index === active ? "active" : ""}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
